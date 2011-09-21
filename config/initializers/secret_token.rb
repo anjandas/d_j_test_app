0f12ecf36415380ce896e6b956a777c037325eb5
Anjan Das <anjan@anjandas.net>
2011-09-21 11:08:10 -0400

    Initial commit of unmodified new Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DelayedJobSample::Application.config.secret_token = '480f8e6f205075b3f6514011c6549c08aa979894553dbcae20f5350e7423be8961ecf003490af5c91e118a7220c065f7ca8f57e7aecd2999ced308c052274850'
